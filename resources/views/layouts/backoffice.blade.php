362bbb468557ba53c39ac2afe5d32c5ab0882ef3
Muhammad Alfarozi <mr.alfarozy.a.n@gmail.com>
2025-12-17 16:49:49 +0700

    adjust[dashboard] : update default profile picture

--- resources/views/layouts/backoffice.blade.php
+++ resources/views/layouts/backoffice.blade.php
@@ -224,7 +224,7 @@
                 <li class="nav-item navbar-dropdown dropdown-user dropdown">
                   <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                      <img src="/finji/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                      <img src="https://business.finji.app/finji-logo.png" alt class="w-px-40 h-auto" />
                     </div>
                   </a>
                   <ul class="dropdown-menu dropdown-menu-end">
@@ -233,7 +233,7 @@
                         <div class="d-flex">
                           <div class="flex-shrink-0 me-3">
                             <div class="avatar avatar-online">
-                              <img src="/finji/assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                              <img src="https://business.finji.app/finji-logo.png" alt class="w-px-40 h-auto" />
                             </div>
                           </div>
                           <div class="flex-grow-1">
